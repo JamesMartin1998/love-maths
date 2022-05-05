8e71109c97cc6cf6882d28606c29d6dd025ecba2
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2022-05-05 09:56:13 +0000

    add code for answer box

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,9 @@ document.addEventListener("DOMContentLoaded", function(){
 
 /** Generates 2 random numbers between 1 and 25 */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+
     let num1 = Math.floor(Math.random() * 25 ) + 1;
     let num2 = Math.floor(Math.random() * 25 ) + 1;
 
